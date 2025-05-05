21618a878c1d47e5bd39d7adce69a2071f1bfe1f
Ethan Chen <Ethanchen4@gmail.com>
2025-05-04 23:11:06 -0400

    [WIP] working on using the spotify api

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5174;
+const port = process.env.PORT || 5174;
 
 app.use(express.json());
 
-app.post("/artist", (req, res) => {
-  console.log(req.body);
-  res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+app.post("/artist", async (req, res) => {
+  try {
+    const token_response = await fetch(process.env.TOKEN_URI, {
+      method: process.env.TOKEN_METHOD,
+      body: `grant_type=client_credentials&client_id=${process.env.SPOTIFY_CLIENT_ID}&client_secret=${process.env.SPOTIFY_CLIENT_SECRET}`,
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    if (!token_response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const token_json = await token_response.json();
+
+    const artist_response = await fetch(
+      process.env.SPOTIFY_API_URL +
+        `?q=Kpop+${req.body.gender}+${req.body.genre}&type=artist&market=US&limit=1&offset=0`,
+      {
+        method: process.env.SPOTIFY_API_METHOD,
+        headers: {
+          Authorization: `${token_json.token_type} ${token_json.access_token}`,
+        },
+      }
+    );
+
+    const artist_json = await artist_response.json();
+    console.log(artist_json.artists.items[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(error.status);
+  }
+
   res.status(200).json({
     artist: {
       name: "TWICE",
